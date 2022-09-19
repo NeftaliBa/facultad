6ddef29cc6348f8397797d0b23169a15fad9fc10
Neftali <neftalibarrera03@gmail.com~>
2022-09-19 10:35:41 -0500

    idk

---
+++ holamundo.java
@@ -0,0 +1,10 @@
+package MyFR;
+public class holamundo{
+    public static void main(String[] args) {   
+        System.out.println("Hola mundo");
+
+        for(int i=0; i<=10;i++)
+            System.out.println(i);
+    
+}
+}
