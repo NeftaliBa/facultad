de22499104eae23d0824cbd2e0bd1f3981e01225
Neftali <neftalibarrera03@gmai.com>
2022-10-07 09:11:16 -0500

    reestructure el codigo

--- ordenamiento_1.c
+++ ordenamiento_1.c
@@ -1,35 +1,42 @@
 #include <stdio.h>
 
-int insertion_sort(int a[],int n){
-	for (int i=1;i<n;i++){
-	int key = a[i];
-	int j= i-1;
-	while (key < a[j] && j >= 0){
-		a[j+1]=a[j];
-		--j;
-	}
-	a[j+1]=key;
+int insertion_sort(int a[], int n)
+{
+	for (int i = 1; i < n; i++)
+	{
+		int key = a[i];
+		int j = i - 1;
+		while (key < a[j] && j >= 0)
+		{
+			a[j + 1] = a[j];
+			--j;
+		}
+		a[j + 1] = key;
 	}
 }
 
-void pantalla (int a[], int n){
-for (int j=0;j<n-1;j++){
-	printf("%d, ", a[j]);	
-}
-printf("%d\n", a[n-1]);
+void pantalla(int a[], int n)
+{
+	for (int j = 0; j < n - 1; j++)
+	{
+		printf("%d, ", a[j]);
+	}
+	printf("%d\n", a[n - 1]);
 }
 
-int main (){
+int main()
+{
 	int cdn, x, a[x];
 	printf("ingrese la cantidad de numeros a ingresar: ");
-	scanf("%d",&cdn);
-	for(x=0; x<cdn; x++){
-	printf("ingrese numero %d: ", x+1);
-	scanf("%d", &a[x]);
+	scanf("%d", &cdn);
+	for (x = 0; x < cdn; x++)
+	{
+		printf("ingrese numero %d: ", x + 1);
+		scanf("%d", &a[x]);
 	}
 	printf("Numero ingresado: ");
-	pantalla(a,x);
-	insertion_sort(a,x);
+	pantalla(a, x);
+	insertion_sort(a, x);
 	printf("numero ordenado: ");
-	pantalla(a,x);
+	pantalla(a, x);
 }
