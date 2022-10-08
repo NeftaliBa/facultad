563e79c3864ca7b550f8ba011c26e775e084d72c
Neftali <neftalibarrera03@gmai.com>
2022-10-07 23:32:39 -0500

    a

--- ordenamiento_1.c
+++ ordenamiento_1.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 int insertion_sort(int a[], int n){
-	for (int i = 1; i < n; i++)
-	{
+	for (int i = 1; i < n; i++){
 		int key = a[i];
 		int j = i - 1;
-		while (key < a[j] && j >= 0)
-		{
+		while (key < a[j] && j >= 0){
 			a[j + 1] = a[j];
 			--j;
 		}
@@ -14,22 +12,21 @@ int insertion_sort(int a[], int n){
 	}
 }
 
-void bubble_sort(int a[], int n){
-	for (int i=1; i < n; i++){
-		for (int j=0; j<n-1; j++){
-		   if (a[j] > a[j+1]){
-			   int temp = a[j];
-			   a[j] = a[j+1];
-			   a[j+1] = temp;
-					
-		   }
+void bubble_sort(int a[], int n){	
+	for (int i = 1; i < n; i++)
+	{	for (int j = 0; j < n - 1; j++)
+		{	if (a[j] > a[j + 1]){
+				int temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
 		}
 	}
 }
 
-
 void pantalla(int a[], int n){
-	for (int j = 0; j < n - 1; j++){
+	for (int j = 0; j < n - 1; j++)
+	{
 		printf("%d, ", a[j]);
 	}
 	printf("%d\n", a[n - 1]);
@@ -39,15 +36,16 @@ int main(){
 	int cdn, x, a[x];
 	printf("ingrese la cantidad de numeros a ingresar: ");
 	scanf("%d", &cdn);
-	for (x = 0; x < cdn; x++){
+	for (x = 0; x < cdn; x++)
+	{
 		printf("ingrese numero %d: ", x + 1);
 		scanf("%d", &a[x]);
 	}
 	printf("Numero ingresado: ");
 	pantalla(a, x);
-	bubble_sort(a,x);
+	bubble_sort(a, x);
 	printf("numero ordenado con bubble: ");
-	pantalla(a,x);
+	pantalla(a, x);
 	insertion_sort(a, x);
 	printf("numero ordenado con insertion: ");
 	pantalla(a, x);
